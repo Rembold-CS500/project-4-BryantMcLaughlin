a9a0f2254a8586545788d663dd697785282a3cbe
“BryantMcLaughlin” <Bryant.mclaughlin6@gmail.com>
2024-08-07 14:46:43 -0700

    Added in Milestone 3

--- fingerprint_verification.py
+++ fingerprint_verification.py
@@ -35,6 +35,25 @@ def read_print(filename):
 
 def simple_check(fingerprint1, fingerprint2):
     return fingerprint1['fingerprint'] == fingerprint2['fingerprint']
+
+def variant_check(fingerprint1, fingerprint2, threshold=95.0):
+    if fingerprint1['width'] != fingerprint2['width'] or fingerprint1['height'] != fingerprint2['height']:
+        return False
+
+    match_count = 0
+    total_pixels = 0
+
+    for i in range(7, fingerprint1['height']):
+        for j in range(fingerprint1['width']):
+            if j < len(fingerprint1['fingerprint'][i]) and j < len(fingerprint2['fingerprint'][i]):
+                if fingerprint1['fingerprint'][i][j] == fingerprint2['fingerprint'][i][j]:
+                    match_count += 1
+                total_pixels += 1
+
+    match_percentage = (match_count / total_pixels) * 100
+    print(f"Matching percentage: {match_percentage:.2f}%") # I added to two decimals so that it is a cleaner percentage
+
+    return match_percentage >= threshold
     
 
 # Test the function with the provided files
@@ -50,3 +69,17 @@ print(fingerprint2)
 print(simple_check(fingerprint1, fingerprint1))  # Should return True
 print(simple_check(fingerprint1, fingerprint2))  # Should return False
     # Confirmed that this is functioning as intended
+
+# Test the variant_check function
+print(variant_check(fingerprint1, fingerprint1))  # Should return True if the match percentage is above 95% (which it should since they are the same fingerprint)
+    # Not returning true
+
+# Additional test cases for shifted variant and variant
+filename3 = 'prints/User1_ShiftedVariant1.txt'
+fingerprint3 = read_print(filename3)
+print(variant_check(fingerprint1, fingerprint3))  # Should return False if the match percentage is below 95%
+
+filename4 = 'prints/User1_Variant1.txt'
+fingerprint4 = read_print(filename4)
+print(variant_check(fingerprint1, fingerprint4))  # Should return True if the match percentage is above 95%
+# Not returning true
